0dbf07edbe9e0c396795ddea53d5a4f4f12588f6
Libardo <laamesquitao@ufpso.edu.co>
2021-12-29 15:59:27 -0500

    IMPLEMENTACION - Ocultar barra de navegacion segunda activity.
    - Se realiza este mismo paso para la segunda vista donde no queremos mostrar la barra de navegación.

--- app/src/main/java/com/example/api/Splash.java
+++ app/src/main/java/com/example/api/Splash.java
@@ -1,11 +1,13 @@
 package com.example.api;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,6 +26,11 @@ public class Splash extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -32,11 +39,11 @@ public class Splash extends AppCompatActivity {
         Persona element = (Persona) getIntent().getSerializableExtra("Persona");
         tv1splash = findViewById(R.id.tv1splash);
         tv2splash = findViewById(R.id.tv2splash);
-        iconImageView = findViewById(R.id.iconImageView);
+        iconImageView = findViewById(R.id.imageView);
 
         tv1splash.setText(element.getId());
         tv2splash.setText(element.getTitle());
-        //iconImageView.setImageIcon(Icon );
+        iconImageView.setImageIcon(element.getIconImage());
     }
 
     public void regresar(View view){
