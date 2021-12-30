27a3bb8dd1d721bd75220be6ad224cea836a826e
Libardo <laamesquitao@ufpso.edu.co>
2021-12-30 09:44:53 -0500

    AJUSTE - Se cambia el nombre de la Activity conocida como Splash por elementos.
    - Se modifica el nombre de la activity, con su respectivo xml.
    - Se cambia la funcionalidad del boton, regresar para que no guarde memoria en la caché.
    - Se cambio los nombes de los textview.

--- app/src/main/java/com/example/api/Elementos.java
+++ app/src/main/java/com/example/api/Elementos.java
@@ -47,7 +47,6 @@ public class Splash extends AppCompatActivity {
     }
 
     public void regresar(View view){
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        onBackPressed();
     }
 }
